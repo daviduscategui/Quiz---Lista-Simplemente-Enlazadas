30df6f7141ab893af624a5c9a890ae5c9f8ef25a
daviduscategui <uscategui.dav07@gmail.com>
2026-03-08 21:36:34 -0500

    quiz_estructuras.py

--- quiz_estructuras.py
+++ quiz_estructuras.py
@@ -1,89 +1,78 @@
 class Nodo:
-	def __init__(self, data):
-		self.data = data
-		self.siguiente = None
+    def __init__(self, data):
+        self.data = data
+        self.siguiente = None
 
-# CLase Listas enlazada simple
+# Clase Lista enlazada simple
 class ListaSE:
-	def __init__(self):
-		self.cabeza = None
-  
-  	# Lista Vacia
-	def vacio(self):
-		if self.cabeza == None:
-			print("Está vacia")
-		else:
-			print("Lista no vacia")
+    def __init__(self):
+        self.cabeza = None
 
-	# Agregar al inicio
-	def agregarInicio(self, data):
-		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
-			return
-		else:
-			nuevo_nodo.siguiente = self.cabeza
-			self.cabeza = nuevo_nodo
-       
-    #Contador 
-		contador=0
-		def contador(self):
-			if self.cabeza==None:
-				return 0
-			else:
-				nodoesp=self.cabeza
-				while nodoesp!=None:
-					contador+=1
-					nodoesp=nodoesp.siguiente
-					return contador
+    # Lista Vacía
+    def vacio(self):
+        if self.cabeza is None:
+            print("Está vacía")
+        else:
+            print("Lista no vacía")
 
-	# Eliminar nodo index
-	def eliminar_index(self, index):
-		if self.cabeza == None:
-			return
-		nodo_actual = self.cabeza
-		posicion = 0
-		if posicion == index:
-			self.eliminarInicio()
-		else:
-			while(nodo_actual != None and posicion+1 != index):
-				posicion = posicion+1
-				nodo_actual = nodo_actual.siguiente
-			if nodo_actual != None:
-				nodo_actual.siguiente = nodo_actual.siguiente.siguiente
-			else:
-				print("Nodo no encontrado")
+    # Agregar al inicio
+    def agregarInicio(self, data):
+        nuevo_nodo = Nodo(data)
+        if self.cabeza is None:
+            self.cabeza = nuevo_nodo
+        else:
+            nuevo_nodo.siguiente = self.cabeza
+            self.cabeza = nuevo_nodo
 
-    #Buscar un elemento
-	def buscador(self,data):
-		cent=input(int("Ingrese el elemento a buscar: "))
-		if self.cabeza==None:
-			return False
-		else:
-			nodoesp=self.cabeza
-			while nodoesp!=cent:
-				nodoesp=nodoesp.siguiente
-				if nodoesp==None:
-					return False
-      
-	# Imprimir Lista
-	def imprimir(self):
-		nodo_actual = self.cabeza
-		while(nodo_actual):
-			print(nodo_actual.data)
-			nodo_actual = nodo_actual.siguiente
+    # Contador
+    def contador(self):
+        contador = 0
+        nodoesp = self.cabeza
+        while nodoesp is not None:
+            contador += 1
+            nodoesp = nodoesp.siguiente
+        return contador
 
-#Huespedes
-def huespedes(self,data):
+    # Buscar un elemento
+    def buscador(self, data):
+        nodoesp = self.cabeza
+        while nodoesp is not None:
+            if nodoesp.data == data:
+                return True
+            nodoesp = nodoesp.siguiente
+        return False
+    
+    #Buscador posicion
+    def buscapos(self,data):
+        nodoesp=self.cabeza
+        pos=0
+        while nodoesp.data is not None:
+            if nodoesp.data==data:
+                return pos
+            nodoesp=nodoesp.siguiente
+            pos+=1
+
+    # Imprimir Lista
+    def imprimir(self):
+        nodo_actual = self.cabeza
+        while nodo_actual:
+            print(nodo_actual.data)
+            nodo_actual = nodo_actual.siguiente
+
+    #Vaciar
+    def vaciar(self):
+        self.cabeza=None
+
+    #Huespedes
+    def huespedes(self,data):
         print("Seleccione una opcion")
         opc=input(int(f"1. Consulta de huespedes, /n, 2. Consulta de habitaciones"))
         if opc==1:
             opc2=input(int(f"1. Individual, /n, 2. Total"))
             if opc2==1:
-                print("Ingrese el nombre del cliente")
-                nombre=input()
+                nombre=str(input("Ingrese el nombre del cliente: "))
                 if self.buscador(nombre):
-                    print(f"El cliente {nombre} tiene {self.contador()} huespedes")
+                    print(f"El cliente {nombre} esta hospedado")
                 else:
                     print("El cliente no existe")
             elif opc2==2:
@@ -105,28 +94,95 @@ def huespedes(self,data):
                 print("El total de habitaciones ocupadas es: ",self.contador())
         else:
             print("Opcion no valida")
-            
-lista = ListaSE()
+
+listaH1=ListaSE()
+listaH2=ListaSE()
+listaH3=ListaSE()
+listaH4=ListaSE()
+listaH5=ListaSE()
 
 Opc=0
 while Opc!=6:
     print("Seleccione una opcion")
-    Opc=int(input(f"1. Agregar huespedes, 2. Quitar huespedes, 3. Consultar huespedes, 4. Quitar habitaciones, 5. Consultar habitaciones, 6. Salir "))
+    Opc=int(input(f"1. Agregar huespedes, 2. Quitar huespedes, 3. Consultar huespedes, 4. Quitar habitaciones, 5. Consultar habitaciones, 6. Salir: "))
     if Opc==1:
-        nom=str(input("Ingrese el nombre del cliente: "))
-        lista.agregarInicio(nom)
-        ced=int(input("Ingrese el numero de cedula: "))
-        lista.agregarInicio(ced)
-        lista.imprimir()
+        centi=1
+        while centi!=2:
+            Nhab=int(input("En que habitacion se va a hospedar?: "))
+            if Nhab==1:
+                ced=int(input("Ingrese el numero de cedula: "))
+                listaH1.agregarInicio(ced)
+                nom=str(input("Ingrese el nombre del cliente: "))
+                listaH1.agregarInicio(nom)
+                listaH1.agregarInicio(Nhab)
+            elif Nhab==2:
+                ced=int(input("Ingrese el numero de cedula: "))
+                listaH2.agregarInicio(ced)
+                nom=str(input("Ingrese el nombre del cliente: "))
+                listaH2.agregarInicio(nom)
+                listaH2.agregarInicio(Nhab)
+            elif Nhab==3:
+                ced=int(input("Ingrese el numero de cedula: "))
+                listaH3.agregarInicio(ced)
+                nom=str(input("Ingrese el nombre del cliente: "))
+                listaH3.agregarInicio(nom)
+                listaH3.agregarInicio(Nhab)
+            elif Nhab==4:
+                ced=int(input("Ingrese el numero de cedula: "))
+                listaH4.agregarInicio(ced)
+                nom=str(input("Ingrese el nombre del cliente: "))
+                listaH4.agregarInicio(nom)
+                listaH4.agregarInicio(Nhab)
+            elif Nhab==5:
+                ced=int(input("Ingrese el numero de cedula: "))
+                listaH5.agregarInicio(ced)
+                nom=str(input("Ingrese el nombre del cliente: "))
+                listaH5.agregarInicio(nom)
+                listaH5.agregarInicio(Nhab)
+            else:
+                print("Habitacion no encontrada")
+            centi=int(input("1. Continuar, 2. Salir: "))
+        print("Habitacion 1")
+        listaH1.imprimir()
+        print("Habitacion 2")
+        listaH2.imprimir()
+        print("Habitacion 3")
+        listaH3.imprimir()
+        print("Habitacion 4")
+        listaH4.imprimir()
+        print("Habitacion 5")
+        listaH5.imprimir()
     elif Opc==2:
         qui=int(input("Ingrese la cedula del huesped que desea quitar: "))
-        lista.eliminar_index(qui)
-        lista.imprimir()
+        print(f"La posicion del huesped en la lista es de {lista.buscapos(qui)}")
+        lista.eliminaresph(qui)
     elif Opc==3:
-        lista.huespedes()
+        Nhab=int(input("Que habitacion quiere consultar?: "))
+        if Nhab==1:
+            listaH1.huespedes()
+        elif Nhab==2:
+            listaH2.huespedes()
+        elif Nhab==3:
+            listaH3.huespedes()
+        elif Nhab==4:
+            listaH4.huespedes()
+        elif Nhab==5:
+            listaH5.huespedes()
+        else:
+            print("Habitacion no encontrada")
     elif Opc==4:
-        print("VA")
-    
-lista.agregarInicio(10)
-lista.agregarInicio(20)
-print(lista.vacio())
+        qui=int(input("Ingrese la habitacion que desea quitar: "))
+        if qui==1:
+            listaH1.vaciar()
+        elif qui==2:
+            listaH2.vaciar()
+        elif qui==3:
+            listaH3.vaciar()
+        elif qui==4:
+            listaH4.vaciar()
+        elif qui==5:
+            listaH5.vaciar()
+        else:
+            print("Habitacion no encontrada")
+    elif Opc==5:
+        lista.huespedes()
